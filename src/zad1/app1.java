3ed7d69de42663ea00d0b4cc078cd9c303aaa348
JacobNatural <jakubmarjankowski@gmail.com>
2024-03-04 20:57:38 +0100

    b4 1 b4 2

--- src/zad1/app1.java
+++ src/zad1/app1.java
@@ -2,7 +2,7 @@ package zad1;
 
 public class app1 {
     public static void main(String[] args) {
-        System.out.println("b2 v1");
-        System.out.println("xxxx");
+        System.out.println("b4 1");
+        System.out.println("b4 2");
     }
 }
